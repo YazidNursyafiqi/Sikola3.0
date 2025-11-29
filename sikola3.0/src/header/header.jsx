2314b9c948fa1c1e3a165eeaf04f7433bd3fe6c8
Yazid nursyafiqi <yazidnursyafiqi007@gmail.com>
2025-11-29 23:15:57 +0800

    nyelesain header dan start buat navbar

--- sikola3.0/src/header/header.jsx
+++ sikola3.0/src/header/header.jsx
@@ -8,9 +8,10 @@ function Header(){
             </div>
 
             <div id={styles.header_right_side}>
+                <img src='/icons/header/header_day_and_night_icon.png' alt='enable/disabled dark mode' id={styles.darkmode_button}/>
                 <a href=''>Home</a>
                 <a href=''>Contacts</a>
-                <img src='/icons/header/header_profile_icon.png'/>
+                <img src='/icons/header/header_profile_icon.png' alt='account' id={styles.account_button}/> 
             </div>
         </div>
     );
